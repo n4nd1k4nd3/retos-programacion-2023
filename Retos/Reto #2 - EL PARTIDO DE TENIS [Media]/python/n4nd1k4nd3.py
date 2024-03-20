7cda060a0342377be139e8cd80529b2d59f5ad96
n4nd1 <nand1kande@gmail.com>
2024-03-20 10:19:27 +0100

    nomes_falta_la_ventaja

--- Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/python/n4nd1k4nd3.py
+++ Retos/Reto #2 - EL PARTIDO DE TENIS [Media]/python/n4nd1k4nd3.py
@@ -18,7 +18,32 @@
 '''
 
 def resultado_tenis(seq):
-    pass
+    zero = 'Love'
+    empate = 'Deuce'
+    player_1 = 0
+    player_2 = 0
+    ganador = ""
+
+    for i in seq:
+        if i == 'P1':
+           player_1 += 15
+        else:
+            player_2 += 15
+        # variables marcadores
+        marcador_1 = zero if player_1 == 0 else player_1
+        marcador_2 = zero if player_2 == 0 else player_2
+        if marcador_1 == marcador_2:
+           print(empate)
+        else:    
+            print(f"{marcador_1} - {marcador_2}")
+    if player_1 > player_2:
+        ganador = "P1"
+    else:
+        ganador = "P2"  
+      
+    return f"Ha ganado el {ganador}"
+
+# probes
 
 secuencia1 = ['P1', 'P1', 'P2', 'P2', 'P1', 'P2', 'P1', 'P1']
 proba1 = resultado_tenis(secuencia1)
